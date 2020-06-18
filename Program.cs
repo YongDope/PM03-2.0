ba811e226b0d8ca20a445032deaea625c617ddf4
YongDope <64275061+YongDope@users.noreply.github.com>
2020-06-18 14:28:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM03console
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Введите кол-во городов:");
+                int count = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\n");
+                Town[] town = new Town[count];
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine("Введите Название города:");
+                    var name = Console.ReadLine();
+                    Console.WriteLine("Введите Количество жителей:");
+                    int kolvo = Convert.ToInt32( Console.ReadLine());
+                    Console.WriteLine("Введите Площадь территории города");
+                    int ploshad = Convert.ToInt32 (Console.ReadLine());
+                    town[i] = new Town(name, kolvo, ploshad, count);
+                }
+                town = Town.Sort(town);
+                foreach (var towns in town)
+                {
+                    Console.WriteLine(town.ToString());
+                }
+                Console.WriteLine("Введите путь для сохранения файлов");
+                var path = Console.ReadLine();
+                path = "Text.txt";
+                Town.SaveAs(path, town);
+                Console.ReadLine();
+            }
+            catch (Exception exe)
+            {
+                Console.WriteLine(exe.Message);
+            }
+
+        }
+    }
+}
+
---
+++ Town.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PM03console
+{
+
+    class Town : Object
+    {
+        private string name;
+        private int kolvo;
+        private int ploshad;
+        private int count;
+
+        public string Name
+        {
+            get => name; set => name = value;
+        }
+        public int Kolvo
+        {
+            get => kolvo; set => kolvo = value;
+        }
+        public int Ploshad
+        {
+            get => ploshad; set => ploshad = value;
+        }
+        public Int32 Count
+        {
+            get => count; set => count = value;
+        }
+        public Town(string name, int kolvo, int ploshad, int count)
+        {
+            this.Name = name;
+            this.Kolvo = kolvo;
+            this.Ploshad = ploshad;
+            this.count = count;
+        }
+        public Town()
+        {
+            this.Name = "";
+            this.Kolvo = 0;
+            this.Ploshad = 0;
+        }
+        public static Town[] Sort(Town[] town)
+        {
+            {
+
+                for (int i = 0; i < town.Length; i++)
+                {
+                    for (int j = i + 1; j < town.Length; j++)
+                    {
+                      
+                            if (town[i].ploshad > town[j].ploshad)
+                            {
+                                var temp = town[i].ploshad;
+                            town[i].ploshad = town[j].ploshad;
+                            town[j].ploshad = temp;
+                            }
+                    }
+                }
+
+                return town;
+            }
+        }
+
+        public static bool SaveAs(string path, Town[] gorod)
+        {
+            if (File.Exists(path.ToString()))
+            {
+
+                Console.WriteLine("Файл уже существует,перезаписать файл?(Y/N)");
+                string answer = Console.ReadLine();
+                if (answer == "y" || answer == "Y" || answer == "у" || answer == "У" || answer == "д" || answer == "Д")
+                {
+                    File.Delete(path);
+                    TextWriter opnFile = new StreamWriter(path);
+                    foreach (var tow in gorod)
+                    {
+                        opnFile.WriteLine(gorod.ToString());
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                using (TextWriter opnFile = new StreamWriter(path.ToString()))
+                {
+                    foreach (var tow in gorod)
+                    {
+                        opnFile.WriteLine(tow.ToString());
+                    }
+                    opnFile.Close();
+                }
+                return true;
+            }
+        }
+        public override string ToString()
+        {
+            return "Название: " + Name +  "Площадь:" + Ploshad + "\nКоличество жителей:" + Kolvo+ "\n";
+        }
+
+    }
+}
+
